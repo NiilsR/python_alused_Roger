6a5adb49bb7475b548291d6be439ef56f86f348b
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-02-03 21:14:07 +0200

    Update Harjutus04.py

--- Harjutus04.py
+++ Harjutus04.py
@@ -2,11 +2,33 @@
 # Roger Niils
 #03.02.22
 
+'''
+hind = int(input("Toote hind: "))
+
+if hind <= 10:
+    hind = hind * 0.9
+else:
+    hind = hind * 0.8
+    
+print(f"Toote hind on {hind}")
+'''
+
+'''
+# juubel
+# kasutaja sisestab oma sünnipäeva ja programm ütleb, kas on juubel või ei.
+
+sunnipaev = input("Sisesta enda sünnipäev: ",)
+if int(sunnipaev) % 10 == 0:
+    print("JAH")
+else:
+    print("EI")
+'''
+
+'''
 #matemaatika
 arv1 = int(input("Sisesta mingi arv: "))
 tehe = input("Sisestage mingi tehe (+,-,*,/)")
 arv2 = int(input("Sisesta mingi arv: "))
-
 
 if tehe == "+":
     v = arv1+arv2
@@ -16,16 +38,17 @@ elif tehe == "*":
     v = arv1*arv2
 else:
     v = arv1/arv2
-print("{arv1}{tehe}{arv2}={v}")
-    
+print(f'{arv1}{tehe}{arv2}={v}')
+'''
 
-
+'''
 #Ruudu kontroll == > < >= !=
 a = int(input("Sisesta ruudu üks külg: "))
 b = int(input("Sisesta ruudu teine külg: "))
 if a == b:
     print("Tegemist on ruuduga")
-#elif:
+elif:
 # kirjutad siia veel midagi
 else:
     print("Risttahukas")
+'''
