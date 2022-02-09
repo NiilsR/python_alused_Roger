3de523ba7d507466b2eb5cfb3435c06c8fa273f5
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-02-09 14:05:06 +0200

    Update Harjutus04.py

--- Harjutus04.py
+++ Harjutus04.py
@@ -1,27 +1,107 @@
 # Harjutus04
 # Roger Niils
-#03.02.22
+# 03.02.22
+
+# arvamismäng
 
 '''
-hind = int(input("Toote hind: "))
+import random
+kord = 0
+loop = 1
+suvnr = random.randint(1, 11)
+while loop == 1:
+    if kord == 2:
+        loop = 0
+    tasku = int(input("Sisesta arv 1-10 "))
+    kord +=1
+    if tasku == suvnr:
+        print("tubli")
+        loop = 0
+    else:
+        print("ei arvand")
+'''        
+'''
+# viisikud
 
+for i in range(1,101):
+    if i % 5 == 0:
+        print(i)
+'''
+    
+'''
+# korrutustabel
+
+number = 5
+
+for i in range(1, 10):
+    print(number, 'x', i, '=', number*i)
+'''
+
+'''
+# paaris ja paaritu
+
+number = input("Sisesta enda number: ",)
+if int(number) % 2 == 0:
+    print("{0} on paaris".format(number))
+else:
+    print("{0} on paaritu".format(number))
+'''
+
+'''
+# loto
+
+import random
+suvalinenr = random.randint(10000,99999)
+print(f"Siin on sinu suvaline number: {suvalinenr}")
+'''
+
+'''
+# tärnid
+
+for i in range(1,6):
+    for j in range(1,6):
+        print('* ', end='')
+    print()
+print("-----------------------------")
+for k in range(0,5):
+    for l in range(0, k+1):
+        print ('*', end='')
+    print()
+print("-----------------------------")
+for m in range(5,0,-1):
+    for n in range(0,m):
+        print('*', end='')
+    print()
+'''
+
+'''
+# jalgpall
+
+sugu = input("Sisesta sugu: ")
+if sugu == "mees":
+    vanus = int(input("Sisesta vannus: "))
+    if vanus >=16 and vanus <=18:
+        print("Olete sobilik")
+'''
+'''
+# müük
+hind = int(input("Toote hind: "))
 if hind <= 10:
     hind = hind * 0.9
 else:
     hind = hind * 0.8
     
-print(f"Toote hind on {hind}")
+print(f"Toote hind on {hind}€")
 '''
 
 '''
 # juubel
 # kasutaja sisestab oma sünnipäeva ja programm ütleb, kas on juubel või ei.
-
 sunnipaev = input("Sisesta enda sünnipäev: ",)
 if int(sunnipaev) % 10 == 0:
-    print("JAH")
+    print("On juubel")
 else:
-    print("EI")
+    print("Ei ole juubel")
 '''
 
 '''
@@ -29,7 +109,6 @@ else:
 arv1 = int(input("Sisesta mingi arv: "))
 tehe = input("Sisestage mingi tehe (+,-,*,/)")
 arv2 = int(input("Sisesta mingi arv: "))
-
 if tehe == "+":
     v = arv1+arv2
 elif tehe == "-":
