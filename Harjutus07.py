52b49cde3fa65998c9873b2d0926c9c2cab088df
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-02-16 09:44:55 +0200

    Add files via upload

---
+++ Harjutus07.py
@@ -0,0 +1,24 @@
+# Roger Niils
+# Harjutus07
+# 16.02.22
+
+# ruumala
+
+
+
+# tervita
+
+'''
+nimi = input("Sisestage nimi: ")
+keel = input("Sisestage enda keel et,en,de : ")
+
+def tervita(n,k="de"):
+    if k == "en":
+        print("Hello", nimi)
+    elif k == "et":
+        print("Tere", nimi)
+    elif k == "de":
+        print("Hallo", nimi)  
+    
+tervita(nimi,keel)
+'''
