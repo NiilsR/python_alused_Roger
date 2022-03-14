1e10d344cd3d67b3ef9cb76b995dcc451fb20541
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-03-14 13:39:41 +0200

    Add files via upload

---
+++ Iseseisevtöö.py
@@ -0,0 +1,23 @@
+# Roger Niils
+# 14.03.22
+# IT21
+
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(600,600)
+aken.title("Iseseisev töö")
+
+# kujundi loomine
+kk = turtle.Turtle()
+for x in range(1):
+    kk.forward(100)
+    kk.left(90)
+    kk.forward(100)
+    kk.right(90)
+    kk.forward(100)
+    kk.right(90)
+    kk.forward(100)
+    kk.left(90)
+    kk.forward(100)
