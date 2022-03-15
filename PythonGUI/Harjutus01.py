0cc85de72571be8ee8c16dab742d13dfdf753e76
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-03-15 11:10:51 +0200

    Add files via upload

---
+++ PythonGUI/Harjutus01.py
@@ -0,0 +1,20 @@
+# Roger Niils
+# 14.03.22
+# IT21
+
+import turtle
+import random
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Harjutus 01")
+colors = ('red','blue', 'orange', 'green')
+turn = 0
+spikes = 8
+size = 10
+for i in range(spikes):
+    kk = turtle.Turtle()
+    kk.color(random.choice(colors))
+    kk.left(turn)
+    kk.forward(100)
+    turn+=45
---
+++ PythonGUI/Harjutus02.py
@@ -0,0 +1,10 @@
+# viisnurk
+import turtle
+aken = turtle.Screen()
+aken.setup(300,300)
+kk = turtle.Turtle()
+for i in range (0,8):
+    kk.forward(100)
+    kk.right(144)
+
+
